56c904cebc3a992aebbdd027889d720ac929067f
Ashim Sharma <ashimsharma337@gmail.com>
2023-01-18 19:30:24 -0500

    simple program for addition

--- Telusko2023/Hello.java
+++ Telusko2023/Hello.java
@@ -3,7 +3,9 @@ class Hello
     public static void main(String[] args)
     {   
         //we put type of variable infront of java
-        int num = 3456;
-        System.out.println(num);
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println(result);
     }
 }
