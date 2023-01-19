f4ae431f739be7d392954e92528e40a3bb1959be
Ashim Sharma <ashimsharma337@gmail.com>
2023-01-18 19:26:20 -0500

    class in java

---
+++ Telusko2023/Hello.java
@@ -0,0 +1,9 @@
+class Hello
+{
+    public static void main(String[] args)
+    {   
+        //we put type of variable infront of java
+        int num = 3456;
+        System.out.println(num);
+    }
+}
