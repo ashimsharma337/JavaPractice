2d5441444daab5dd42020b335df041477f614a93
Ashim Sharma <ashimsharma337@gmail.com>
2023-02-06 12:57:46 -0500

    relational operator completed

---
+++ Telusko2023/Operators/OperatorOfJava.java
@@ -0,0 +1,50 @@
+
+//Arithmetic operator
+public class OperatorOfJava {
+    
+    public static void main(String[] args)
+    {
+        // int num1 = 7;
+        // int num2 = 5;
+
+        // int result = num1 + num2; //add"+",subtract"-",multiplication"*",division"/"" incase of division only quotient 
+        // int result = num1 / num2;    // output 1 -only quotient 
+        // int result = num1 % num2;       // this gives -only remainder output is 2 "%" modulus operator
+        // System.out.println(result);
+
+        // int num3 = 7;
+        // num3 = num3 + 2;       // output is 9
+        // num3 += 2;                // works with subtrations also 
+
+        // shortcut
+        // num3++;        //incrementing -output 8 post-increment
+        // num3--;        //decrement    -output 6
+        // ++num3;    //output is 8 pre-increment it behaves diffrently when we try to fetch a value
+        // int result = num3++;     //output 7 because because first fetch the value after assignment operator(=) then it will increment
+        // int result = ++num3;     //first increment and then fetch the value
+        // System.out.println(num3);
+        // System.out.println(result);
+
+
+                      //Assignment Operators
+
+       // "="assignment, "<"lessthan, ">" greater than, "=="equal to, "!="not equal to, "<="less than equal to, ">="greater than equal to
+
+       // int x = 6;
+       // int y = 5;
+
+        // boolean result = x < y;    // output false
+        // boolean result = x <= y;   // output false
+        // boolean result = x >= y;      // output true
+
+        // boolean result = x != y;        // output true  // if two values are not equal
+        // boolean result = x == y;           // output false // if two values are equal
+
+
+        // System.out.println(result); 
+
+    }
+
+
+
+}
