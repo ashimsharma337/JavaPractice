e965a62dda24ec38dd9b9d949e5467bd0976d61c
Ashim Sharma <ashimsharma337@gmail.com>
2023-02-08 13:22:02 -0500

    if else conditional completed

--- Telusko2023/Conditional/Conditional.java
+++ Telusko2023/Conditional/Conditional.java
@@ -3,7 +3,7 @@ public class Conditional {
 
     public static void main(String[] args) {
 
-                     //...If Else
+                     //...if else
 
         // int x = 28;
                             // if there is there is one statement inside if we don't need curly bracket.
@@ -27,8 +27,8 @@ public class Conditional {
 
         // another example
 
-        int x = 5;
-        int y = 7;
+        // int x = 5;
+        // int y = 7;
 
         //To print the greatest number 
 
@@ -37,14 +37,32 @@ public class Conditional {
         // else
         //    System.out.println(y);
 
-        if(x<y) {
-           System.out.println(x);     // If there are multiple statement we need curly bracket  
-           System.out.println("Thank you!");  // Prints 5 and thank you.
+        // if(x<y) {
+        //    System.out.println(x);     // If there are multiple statement we need curly bracket  
+        //    System.out.println("Thank you!");  // Prints 5 and thank you.
 
+        // }
+        // else
+        //    System.out.println(y);
+
+                 // if else with three value
+
+        int x = 8;
+        int y = 7;
+        int z = 9;
+
+                 // to print the greatest of three value
+
+        if(x>y && x>z) {
+           System.out.println(x);       
+           System.out.println("Thank you!");  
         }
-        else
+        else if(y>z) {
            System.out.println(y);
-
+        }
+        else {
+            System.out.println(z);
+        }
 
 
     }
