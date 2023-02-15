4aeb81c6c1436d1d0219741b17231e49892b76e1
Ashim Sharma <ashimsharma337@gmail.com>
2023-02-14 20:12:54 -0500

    switch statement completed

--- Telusko2023/Conditional/Conditional.java
+++ Telusko2023/Conditional/Conditional.java
@@ -66,8 +66,8 @@ public class Conditional {
 
         //Print 10 if the n is even and print 20 if n is odd
         //int n = 6;
-        int n = 5;
-        int result = 0;
+        // int n = 5;
+        // int result = 0;
 
         // if(n%2==0)
         //     result = 10;
@@ -77,9 +77,59 @@ public class Conditional {
         //using ternary operator
 
         // ?:
-        result = n%2==0 ? 10 : 20;
+        // result = n%2==0 ? 10 : 20;
         
-        System.out.println(result);
+        // System.out.println(result);
+
+        //switch statement
+
+        // int n = 1;
+        // based on the value of n print days
+        //using if else
+        // if(n==1)
+        // System.out.println("Monday");
+        // else if(n==2)
+        // System.out.println("Tuesday");
+        // else if(n==3)
+        // System.out.println("Wednesday");
+        // else if(n==4)
+        // System.out.println("Thursday");
+        // else if(n==5)
+        // System.out.println("Friday");
+        // else if(n==6)
+        // System.out.println("Saturday");
+        // else
+        // System.out.println("Sunday");
+
+        //using switch case
+        
+        int n =8;
+        
+        switch(n){
+            case 1:
+                System.out.println("Monday"); // without break everything will print after match
+                break;                        // recently in advanced java version we can skip break  
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:      //if non of the case matches use default
+                System.out.println("Enter a valid number!");
+        }
 
     }
 }
