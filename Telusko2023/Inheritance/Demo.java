dd0da133cd63bc8f335b16b976de9c7614878883
Ashim Sharma <ashimsharma337@gmail.com>
2023-02-22 14:23:28 -0500

    multi level inheritance

--- Telusko2023/Inheritance/Demo.java
+++ Telusko2023/Inheritance/Demo.java
@@ -7,11 +7,12 @@ public class Demo {
     
     public static void main(String[] args)
     {
-        AdvCalc obj = new AdvCalc();
+        VeryAdvCalc obj = new VeryAdvCalc();
         int r1 = obj.add(4, 2);
         int r2 = obj.sub(7, 3);
         int r3 = obj.multi(5, 3);
         int r4 = obj.div(15, 4);
-        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
+        double r5 = obj.power(4, 2);
+        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4 + " " + r5);
     }
 }
