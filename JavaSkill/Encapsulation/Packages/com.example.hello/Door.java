4c57e7797bba747f715ea0a262ca9fcae48c5a3a
Ashim Sharma <ashimsharma337@gmail.com>
2023-03-13 12:06:03 -0400

    java package

---
+++ JavaSkill/Encapsulation/Packages/com.example.hello/Door.java
@@ -0,0 +1,9 @@
+package com.example.hello;
+import  com.example.hi;   // key imported from the hi package 
+
+public class Door {
+     
+	Key key;             // has-A relationship 
+	String string;       
+}
+
---
+++ JavaSkill/Encapsulation/Packages/com.example.hello/Frame.java
@@ -0,0 +1,7 @@
+
+
+public class Frame {
+    
+    Door door; // Door belongs to same package so no compilation error 
+    String string; // String belongs to java.lang package so no need to import 
+}
