024b0fa163d839b68aa7bba7567f7c6bdeb04a8f
Ashim Sharma <ashimsharma337@gmail.com>
2023-04-20 14:30:32 -0400

    fullstack app with ajax, servlet, and mysql

---
+++ Full-Stack-Java/HomeBuyerApp/homebuyer/src/main/java/beans/Home.java
@@ -0,0 +1,39 @@
+package beans;
+
+public class Home {
+      
+	private int id;
+	private String address;
+	private int price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "Home [id=" + id + ", address=" + address + ", price=" + price + "]";
+	}
+	public Home() {
+		super();
+	}
+	public Home(String address, int price) {
+		super();
+		this.address = address;
+		this.price = price;
+	}
+}
